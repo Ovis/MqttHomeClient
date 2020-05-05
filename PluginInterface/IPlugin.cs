01ac435da0eb1fd9957851ae0bae1795eba0e4da
Ovis <me+github@hitsujin.jp>
2020-05-05 21:00:13 +0900

    処理をプラグイン化

---
+++ PluginInterface/IPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace PluginInterface
+{
+    public interface IPlugin
+    {
+        string Topic { get; }
+
+        string PluginName { get; }
+
+        bool Action(string text);
+
+        Task<bool> ActionAsync(string text);
+    }
+}
